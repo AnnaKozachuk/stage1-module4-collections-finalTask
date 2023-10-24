7fffb803f1403074daa2e9ad68fcb87194483c85
FlyingElephant135 <46475082+FlyingElephant135@users.noreply.github.com>
2023-10-24 21:04:21 +0300

    commit

--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,17 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> resultMap = new HashMap<>();
+        for (Map.Entry<String, Integer> entry : sourceMap.entrySet()) {
+            int keyLength = entry.getKey().length();
+            resultMap.computeIfAbsent(keyLength, k -> new TreeSet<>()).add(entry.getKey());
+        }
+        return resultMap;
     }
 }
